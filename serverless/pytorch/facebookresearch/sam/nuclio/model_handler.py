270d904b9c6f9c143fc00778d865183a97456f3c
Grigorii Mironov <grigoriinm@gmail.com>
2023-05-05 12:35:50 +0300

    Add simple image caching to the Segment Anything interactor
    Cache size changed from 1 to 10 images
    If you want to change amount of images, change envvar ``AMOUNT_OF_IMAGES_TO_CACHE`` in
    ``serverless\pytorch\facebookresearch\sam\nuclio\function.yaml`` or
    ``serverless\pytorch\facebookresearch\sam\nuclio\function-gpu.yaml``

--- serverless/pytorch/facebookresearch/sam/nuclio/model_handler.py
+++ serverless/pytorch/facebookresearch/sam/nuclio/model_handler.py
@@ -5,7 +5,16 @@
 import numpy as np
 import cv2
 import torch
+import os
+from typing import Optional
 from segment_anything import sam_model_registry, SamPredictor
+
+def index_of_same_image(list_of_images, image):
+    for i, cached_img in enumerate(list_of_images):
+        is_the_same_image = cached_img is not None and np.array_equal(np.array(image), cached_img)
+        if is_the_same_image:
+            return i
+    return None
 
 def convert_mask_to_polygon(mask):
     contours = None
@@ -31,21 +40,25 @@ class ModelHandler:
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.sam_checkpoint = "/opt/nuclio/sam/sam_vit_h_4b8939.pth"
         self.model_type = "vit_h"
-        self.latest_image = None
-        self.latest_low_res_masks = None
+        self.amount_of_images = int(os.environ.get('AMOUNT_OF_IMAGES_TO_CACHE', '10'))
+        self.latest_images = [np.array([]) for _ in range(self.amount_of_images)]
+        self.latest_low_res_masks : list[Optional[np.ndarray]] = [None] * self.amount_of_images
         sam_model = sam_model_registry[self.model_type](checkpoint=self.sam_checkpoint)
         sam_model.to(device=self.device)
-        self.predictor = SamPredictor(sam_model)
+        self.sam_predictors = [SamPredictor(sam_model) for _ in range(self.amount_of_images)]
+        self.last_image_idx = 0
 
     def handle(self, image, pos_points, neg_points):
-        # latest image is kept in memory because function is always run-time after startup
-        # we use to avoid computing emeddings twice for the same image
-        is_the_same_image = self.latest_image is not None and np.array_equal(np.array(image), self.latest_image)
-        if not is_the_same_image:
-            self.latest_low_res_masks = None
+        # latest 'amount_of_images' images are kept in memory because function is always run-time after startup
+        # we use to avoid computing emeddings twice for the cached images
+        idx = index_of_same_image(self.latest_images, image)
+        if idx is None:
+            idx = self.last_image_idx
+            self.latest_low_res_masks[idx] = None
             numpy_image = np.array(image)
-            self.predictor.set_image(numpy_image)
-            self.latest_image = numpy_image
+            self.sam_predictors[idx].set_image(numpy_image)
+            self.latest_images[idx] = numpy_image
+            self.last_image_idx = (self.last_image_idx + 1) % self.amount_of_images
         # we assume that pos_points and neg_points are of type:
         # np.array[[x, y], [x, y], ...]
         input_points = np.array(pos_points)
@@ -55,13 +68,13 @@ class ModelHandler:
             input_points = np.concatenate([input_points, neg_points], axis=0)
             input_labels = np.concatenate([input_labels, np.array([0] * len(neg_points))], axis=0)
 
-        masks, _, low_res_masks = self.predictor.predict(
+        masks, _, low_res_masks = self.sam_predictors[idx].predict(
             point_coords=input_points,
             point_labels=input_labels,
-            mask_input = self.latest_low_res_masks,
+            mask_input = self.latest_low_res_masks[idx],
             multimask_output=False
         )
-        self.latest_low_res_masks = low_res_masks
+        self.latest_low_res_masks[idx] = low_res_masks
         object_mask = np.array(masks[0], dtype=np.uint8)
         cv2.normalize(object_mask, object_mask, 0, 255, cv2.NORM_MINMAX)
         polygon = convert_mask_to_polygon(object_mask)
